731d2dd21b03c020419bf24fa589602ce193841f
HOME\Дом <angiy.kaa555@yandex.ru>
2023-09-08 22:50:58 +0300

    Adding Sem 2 Class

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задание 16
+/*Напишите программу, которая принимает на
+вход два числа и проверяет, является ли одно
+число квадратом другого.*/
+Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine()??"0");//Принимаем первое число
+
+Console.Write("Введите второе число: ");
+int secondNum = int.Parse(Console.ReadLine()??"0");//Принимаем второе число
+
+bool test1 = (firstNum == secondNum * secondNum);
+bool test2 = (secondNum == firstNum * firstNum);
+
+if (test1)
+{
+    Console.WriteLine("Первое число квадрат второго");
+}
+if (test2)
+{
+    Console.WriteLine("Второе число квадрат первого");
+}
+if (!(test1 || test2))
+{
+    Console.WriteLine("Первое число не квадрат второго и второе число не квадрат первого");
+}
---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача 9
+/*Напишите программу, которая выводит
+случайное число из отрезка [10, 99] и показывает
+наибольшую цифру числа.*/
+void Variant1 ()
+{
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10,100);
+    
+    Console.WriteLine("Способ 1");
+    Console.WriteLine(number);    
+
+    int firstDigit = number/10;//Берем первую цифру
+    int secondDigit = number%10;//Берем вторую цифру
+
+    if(firstDigit>secondDigit)//Сравниваем первую и вторую цифру, наибольшую выводим
+    {
+        Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+}
+void Variant2 ()
+{
+    Console.WriteLine("Способ 2");
+
+    System.Random numberGenerator1 = new System.Random();
+    int number1 = numberGenerator1.Next(10, 100);
+    Console.WriteLine(number1);
+    //Сравниваем и выводим наибольшую цифру
+    Console.WriteLine((number1 / 10 > number1 % 10) ? (int)(number1 / 10) : (int)(number1 % 10));
+}
+void Variant3()
+{
+    Console.WriteLine("Способ 3");
+
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10, 100);
+    Console.WriteLine(number);
+    //Преобразуем в массив символов
+    char[] digitChar = number.ToString().ToCharArray();
+    //Выводим наибольший
+    Console.WriteLine((int)digitChar[0]>(int)digitChar[1]?digitChar[0]:digitChar[1]);
+    //<условие>?<вариант1>:<вариант2>
+}
+
+Variant1();
+Variant2();
+Variant3();
