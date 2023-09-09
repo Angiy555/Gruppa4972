032fd91a41709cefe9788c8b282cfcece017e155
HOME\Дом <angiy.kaa555@yandex.ru>
2023-09-09 19:06:40 +0300

    Adding Sem 2 Homework

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿//задание 10
+/*Напишите программу, которая принимает 
+на вход трёхзначное число и на выходе показывает 
+вторую цифру этого числа.*/
+System.Random numberGenerator = new System.Random();
+int num = numberGenerator.Next(100,1000);//Получаем случайное трехзначное число
+
+Console.WriteLine(num);//Выводим его
+
+int twoNum = num%100;//Отбрасываем первую цифру
+int secondDigit = twoNum/10;//Получаем вторую цифру
+//Выводим вторую цифру
+Console.WriteLine("Вторая цифра числа: " + secondDigit);
