e9f426a9cd339858e859cbd9e2fc1a7beb7d49b9
HOME\Дом <angiy.kaa555@yandex.ru>
2023-09-15 20:20:04 +0300

    Adding Sem3 homework

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задание 19
+/*Напишите программу, которая принимает на
+вход пятизначное число и проверяет, является ли оно
+палиндромом.*/
+//Вводим данные
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Проверка на палиндром
+void CheckingPolindrom(int number)
+{
+    int res = number;
+    if(number > 9999 && number < 100000)
+    {
+        int [] polNum = new int [5];
+        for(int i =0; i < 5; i++)
+        {
+            polNum[i] = number%10;
+            number = number/10;
+        }
+        if(polNum[0] == polNum[4] && polNum[1] == polNum[3]) 
+        {
+            PrintResult("Число " + res + " является палиндромом");
+        }
+        else
+        {
+            PrintResult("Число " + res + " не является палиндромом");
+        }
+    }
+    else
+    {
+        PrintResult("Введено не пятизначное число");
+    }
+}
+//Выполнение
+CheckingPolindrom(ReadData("Введите пятизначное число: "));
+
---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задание 23
+/*Напишите программу, которая принимает на
+вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+//Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+//Строим строку из чисел в степени
+string BuildNumLinePow(int n, int pow)
+{
+    string res = "";
+    for(int i = 1; i <= n; i++)
+    {
+        res = res + Math.Pow(i, pow) + " ";
+    }
+    return res;
+}
+
+int n = ReadData("Ведите число: ");
+PrintResult("ККубов числа от 1 до n: " + BuildNumLinePow(n, 3));
